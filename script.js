17b67bb9d6392cb332a39caade7dfdafdb5a8de4
Paul Becker <paulbecker2003@gmail.com>
2023-10-21 17:14:43 +0200

    Added Method which runs 5 rounds of Games. Prompting the player for his choice.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let playerScore = 0;
+let computerScore = 0;
+
+// Starts a 5 round game, displaying winners and loosers at the end
+// and keeping score
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(prompt("Choose between Rock, Paper and Scissors: "), getComputerChoice()));
+    }
+
+    console.log("Player: " + playerScore + " - Computer: " + computerScore);
+    if (playerScore > computerScore) {
+        console.log("Player wins!");
+    } else if (playerScore === computerScore) {
+        console.log("It's a tie!");
+    } else {
+        console.log("Computer wins!")
+    }
+}
 
 // Returns a random choice for the computer
 function getComputerChoice() {
@@ -18,32 +37,41 @@ function playRound(playerSelection, computerSelection) {
     switch (playerSelection) {
         case "ROCK":
             if (computerSelection === "SCISSORS") {
+                playerScore++;
                 return "Player: " + playerSelection + " - Computer: " + computerSelection + " - Congratulations, Player won!";
             } else if (playerSelection === computerSelection) {
                 return "Computer and Player both chose: " + playerSelection + " - Noone wins!";
             } else {
+                computerScore++;
                 return "Player: " + playerSelection + " - Computer: " + computerSelection + " - Oh no :( Player lost";
             }
             break;
 
         case "PAPER":
             if (computerSelection === "ROCK") {
+                playerScore++;
                 return "Player: " + playerSelection + " - Computer: " + computerSelection + " - Congratulations, Player won!";
             } else if (playerSelection === computerSelection) {
                 return "Computer and Player both chose: " + playerSelection + " - Noone wins!";
             } else {
+                computerScore++;
                 return "Player: " + playerSelection + " - Computer: " + computerSelection + " - Oh no :( Player lost";
             }
             break;
 
         case "SCISSORS":
             if (computerSelection === "PAPER") {
+                playerScore++;
                 return "Player: " + playerSelection + " - Computer: " + computerSelection + " - Congratulations, Player won!";
             } else if (playerSelection === computerSelection) {
                 return "Computer and Player both chose: " + playerSelection + " - Noone wins!";
             } else {
+                computerScore++;
                 return "Player: " + playerSelection + " - Computer: " + computerSelection + " - Oh no :( Player lost";
             }
             break;
     }
 }
+
+// Runs the game
+game();
